e074cf2727ff78d05ab05daed9563ad1d505516a
0xPilot <pilot.majors@gmail.com>
2022-01-06 21:21:23 +1000

    add Oparcade contract and remove unused contract

--- contracts/AddressRegistry.sol
+++ contracts/AddressRegistry.sol
@@ -4,18 +4,18 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract AddressRegistry is OwnableUpgradeable {
-    address public oparcade;
-    address public tokenRegistry;
+  address public oparcade;
+  address public tokenRegistry;
 
-    function initialize() public initializer {
-        __Ownable_init();
-    }
+  function initialize() public initializer {
+    __Ownable_init();
+  }
 
-    function updateOparcade(address _oparcade) external onlyOwner {
-        oparcade = _oparcade;
-    }
+  function updateOparcade(address _oparcade) external onlyOwner {
+    oparcade = _oparcade;
+  }
 
-    function updateTokenRegistry(address _tokenRegistry) external onlyOwner {
-        tokenRegistry = _tokenRegistry;
-    }
+  function updateTokenRegistry(address _tokenRegistry) external onlyOwner {
+    tokenRegistry = _tokenRegistry;
+  }
 }
---
+++ contracts/Oparcade.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract Oparcade is Initializable {
+  function initialize() public initializer {}
+}

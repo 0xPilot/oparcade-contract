2a07965f7c40bd031c29a227e2fd860e87ed2edb
0xPilot <pilot.majors@gmail.com>
2022-01-07 21:54:25 +1000

    add IAddressRegistery interface

---
+++ contracts/interfaces/IAddressRegistry.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+interface IAddressRegistry {
+  function oparcade() external view returns (address);
+
+  function tokenRegistry() external view returns (address);
+}

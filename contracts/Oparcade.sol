c11db2b17ef97f53fee475ac1ef474afcd902f6c
0xPilot <pilot.majors@gmail.com>
2022-01-10 22:28:17 +1000

    add claim functionality in Oparcade contract

--- contracts/Oparcade.sol
+++ contracts/Oparcade.sol
@@ -1,20 +1,25 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "./interfaces/IAddressRegistry.sol";
 import "./interfaces/ITokenRegistry.sol";
 
-contract Oparcade is Initializable {
+contract Oparcade is ReentrancyGuardUpgradeable {
+  using ECDSAUpgradeable for bytes32;
   using SafeERC20Upgradeable for IERC20Upgradeable;
 
   IAddressRegistry public addressRegistry;
   ITokenRegistry public tokenRegistry;
   mapping(address => uint256) private userBalance;
+  mapping(bytes => bool) public signatures;
 
   function initialize(address _addressRegistry) public initializer {
+    __ReentrancyGuard_init();
+
     addressRegistry = IAddressRegistry(_addressRegistry);
     tokenRegistry = ITokenRegistry(addressRegistry.tokenRegistry());
   }
@@ -31,5 +36,25 @@ contract Oparcade is Initializable {
     IERC20Upgradeable(_token).safeTransferFrom(msg.sender, address(this), depositTokenAmount);
   }
 
-  function claim(bytes calldata _signature) external {}
+  function claim(
+    address _winner,
+    address _token,
+    uint256 _amount,
+    bytes calldata _signature
+  ) external nonReentrant {
+    // check if signature is already used
+    require(!signatures[_signature], "Already used signature");
+    signatures[_signature] = true;
+
+    // check if msg.sender is the _winner
+    require(msg.sender == _winner, "Only winner can claim");
+
+    // check signer
+    address maintainer = addressRegistry.maintainer();
+    bytes32 data = keccak256(abi.encodePacked(msg.sender, _token, _amount));
+    require(data.toEthSignedMessageHash().recover(_signature) == maintainer, "Wrong signer");
+
+    // transfer tokens to the winner
+    IERC20Upgradeable(_token).safeTransfer(msg.sender, _amount);
+  }
 }

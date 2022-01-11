7d02be36c5e3df3be7800a3e77ab183f62edc571
0xPilot <pilot.majors@gmail.com>
2022-01-11 19:54:54 +1000

    add soldoc for AddressRegistry contract

--- contracts/AddressRegistry.sol
+++ contracts/AddressRegistry.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
+/**
+ * @title AddressRegistry
+ * @notice This stores all addresses in the Oparcade
+ * @author David Lee
+ */
 contract AddressRegistry is OwnableUpgradeable {
   address public oparcade;
   address public tokenRegistry;
@@ -12,14 +17,29 @@ contract AddressRegistry is OwnableUpgradeable {
     __Ownable_init();
   }
 
+  /**
+   * @notice Update Oparcade contract address
+   * @dev onlyOwner
+   * @param _oparcade Oparcade contract address
+   */
   function updateOparcade(address _oparcade) external onlyOwner {
     oparcade = _oparcade;
   }
 
+  /**
+   * @notice Update TokenRegistry contract address
+   * @dev onlyOwner
+   * @param _tokenRegistry TokenRegistry contract address
+   */
   function updateTokenRegistry(address _tokenRegistry) external onlyOwner {
     tokenRegistry = _tokenRegistry;
   }
 
+  /**
+   * @notice Update maintainer address
+   * @dev onlyOwner
+   * @param _maintainer Maintainer address
+   */
   function updateMaintainer(address _maintainer) external onlyOwner {
     maintainer = _maintainer;
   }

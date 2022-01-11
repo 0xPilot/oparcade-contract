fbc36abfb98407aa0fdfb608122d611832ed7032
0xPilot <pilot.majors@gmail.com>
2022-01-11 20:05:43 +1000

    add soldoc for TokenRegistry contract

--- contracts/TokenRegistry.sol
+++ contracts/TokenRegistry.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
+/**
+ * @title TokenRegistry
+ * @notice This stores the token contract addresses available in the Oparcade
+ * @author David Lee
+ */
 contract TokenRegistry is OwnableUpgradeable {
   mapping(address => uint256) public depositTokenAmount;
 
@@ -10,6 +15,13 @@ contract TokenRegistry is OwnableUpgradeable {
     __Ownable_init();
   }
 
+  /**
+   * @notice Update deposit token information
+   * @dev onlyOwner
+   * @dev Only tokens with an amount greater than zero is valid for the deposit
+   * @param _token Token address to allow/disallow the deposit
+   * @param _amount Token amount
+   */
   function updateDepositTokenAmount(address _token, uint256 _amount) external onlyOwner {
     depositTokenAmount[_token] = _amount;
   }

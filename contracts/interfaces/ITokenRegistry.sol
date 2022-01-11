65e00cc695e28a3acee1daad004c7cd326aafd19
0xPilot <pilot.majors@gmail.com>
2022-01-11 21:31:46 +1000

    add soldoc for ITokenRegistry interface

--- contracts/interfaces/ITokenRegistry.sol
+++ contracts/interfaces/ITokenRegistry.sol
@@ -1,6 +1,16 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+/**
+ * @title TokenRegistry Contract Interface
+ * @notice Define the interface used to get the token information
+ * @author David Lee
+ */
 interface ITokenRegistry {
+  /**
+   * @notice Provide the deposit amount of the token given
+   * @param _token Token address
+   * @return uint256 Deposit amount
+   */
   function depositTokenAmount(address _token) external view returns (uint256);
 }

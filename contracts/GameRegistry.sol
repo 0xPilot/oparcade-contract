0a9f2434ba8976fe44fc8d0c5f1656597bf2a5b1
0xPilot <pilot.majors@gmail.com>
2022-01-13 12:21:48 +1000

    add events and soldoc in GameRegistry

--- contracts/GameRegistry.sol
+++ contracts/GameRegistry.sol
@@ -11,14 +11,23 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 contract GameRegistry is OwnableUpgradeable {
   event AddGame(uint256 indexed gid, string gameName);
   event RemoveGame(uint256 indexed gid, string gameName);
+  event UpdateDepositAmount(uint256 indexed gid, address indexed token, uint256 amount);
+  event UpdateClaimableAmount(uint256 indexed gid, address indexed token, bool isClaimable);
 
+  /// @dev Game name array
   string[] public games;
+
+  /// @dev Game ID -> Token address -> Deposit amount
   mapping(uint256 => mapping(address => uint256)) public depositAmount;
+
+  /// @dev Game ID -> Token address -> Bool
   mapping(uint256 => mapping(address => bool)) public claimable;
+
+  /// @dev Game ID -> Bool
   mapping(uint256 => bool) public isDeprecatedGame;
 
   modifier onlyValidGID(uint256 _gid) {
-    require(_gid < games.length, "Invalid game ID");
+    require(_gid < games.length, "Invalid game index");
     _;
   }
 
@@ -26,6 +35,10 @@ contract GameRegistry is OwnableUpgradeable {
     __Ownable_init();
   }
 
+  /**
+   * @notice Add new game
+   * @param _gameName Game name to add
+   */
   function addGame(string memory _gameName) external onlyOwner returns (uint256 gid) {
     games.push(_gameName);
     gid = games.length - 1;
@@ -33,6 +46,10 @@ contract GameRegistry is OwnableUpgradeable {
     emit AddGame(gid, _gameName);
   }
 
+  /**
+   * @notice Remove game
+   * @dev Game is not removed from the games array, just set it deprecated
+   */
   function removeGame(uint256 _gid) external onlyOwner onlyValidGID(_gid) {
     // remove game
     isDeprecatedGame[_gid] = true;
@@ -54,6 +71,8 @@ contract GameRegistry is OwnableUpgradeable {
     uint256 _amount
   ) external onlyOwner onlyValidGID(_gid) {
     depositAmount[_gid][_token] = _amount;
+
+    emit UpdateDepositAmount(_gid, _token, _amount);
   }
 
   /**
@@ -69,8 +88,13 @@ contract GameRegistry is OwnableUpgradeable {
     bool _isClaimable
   ) external onlyOwner onlyValidGID(_gid) {
     claimable[_gid][_token] = _isClaimable;
+
+    emit UpdateClaimableAmount(_gid, _token, _isClaimable);
   }
 
+  /**
+   * @notice Returns the number of games added in games array
+   */
   function gameLength() external view returns (uint256) {
     return games.length;
   }

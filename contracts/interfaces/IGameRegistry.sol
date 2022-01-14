eb65b9f91c9733e9df7bb5544967e89457582507
0xPilot <pilot.majors@gmail.com>
2022-01-14 20:07:07 +1000

    update IGameRegistry interface

--- contracts/interfaces/IGameRegistry.sol
+++ contracts/interfaces/IGameRegistry.sol
@@ -14,4 +14,24 @@ interface IGameRegistry {
    * @return uint256 Deposit amount
    */
   function depositAmount(uint256 _gid, address _token) external view returns (uint256);
+
+  /**
+   * @notice Provide the claimability of the token given
+   * @param _gid Game ID
+   * @param _token Token address
+   * @return bool true: claimable, false: not claimable
+   */
+  function claimable(uint256 _gid, address _token) external view returns (bool);
+
+  /**
+   * @notice Provide whether the game is deprecated or not
+   * @param _gid Game ID
+   * @return bool true: deprecated, false: not deprecated
+   */
+  function isDeprecatedGame(uint256 _gid) external view returns (bool);
+
+  /**
+   * @notice Returns the number of games added in games array
+   */
+  function gameLength() external view returns (uint256);
 }

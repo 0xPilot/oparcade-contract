743195a124424afb27379c84c4db01c4cb4476bd
0xPilot <pilot.majors@gmail.com>
2022-05-25 11:22:14 +1000

    update IOparcade interface

---
+++ contracts/interfaces/IOparcade.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title Oparcade Contract Interface
+ * @notice Define the interface used to get the token deposit and withdrawal info
+ * @author David Lee
+ */
+interface IOparcade {
+  /**
+   * @notice Provide the Oparcade platform fee
+   * @dev Max value is 1000 (100%)
+   * @return uint256 Oparcade platform fee
+   */
+  function platformFee() external view returns (uint256);
+}
